d65ca5dcbbeec65fae9a16337d8ed2a8979ed6c1
unknown <dryldeogracias@gmail.com>
2023-01-23 18:04:04 +0800

    initialized git repository

---
+++ signatureDeo.js
@@ -0,0 +1,16 @@
+const swiperr = new Swiper('.swiper', { 
+
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        type: "fraction",
+      },
+  
+  });
+
+ 
